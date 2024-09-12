77b6fe70a4091fdc2c91772b5b87d24eff009c77
Shubhangi <shubhangithube806@gmail.com>
2024-09-12 13:59:33 +0530

    ActionMethods are added.

---
+++ ASP.NET_MVC_Learning/Controllers/ActionMethodController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Learning.Controllers
+{
+    public class ActionMethodController : Controller
+    {
+        // GET: ActionMethod
+        public ActionResult Index()  //Action method cha return type ActionResult ahe.
+        {
+            return View();
+        }
+
+        public string Show()  //
+        {
+            return "This is a second action method of actionmethod controller";
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public int StudentId(int id)
+        {
+            return id;
+        }
+
+    }
+}
