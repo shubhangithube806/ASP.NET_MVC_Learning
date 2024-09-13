1d830574f6331d0ddc1a61c8b3046caf371cbf4a
Shubhangi <shubhangithube806@gmail.com>
2024-09-13 09:10:29 +0530

    ViewData page is added.

---
+++ ASP.NET_MVC_Learning/Controllers/ViewDataController.cs
@@ -0,0 +1,44 @@
+﻿using ASP.NET_MVC_Learning.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Learning.Controllers
+{
+    public class ViewDataController : Controller
+    {
+        // GET: ViewData - get data from controller and send to the view
+        public ActionResult Index()
+        {
+            //pass single value in ViewData's object
+            ViewData["Message"] = "Message from View Data !!";  //here Message is the ViewData's Object
+            ViewData["CurrentTime"] = DateTime.Now.ToLongTimeString();
+
+            //pass array in ViewData's object
+            string[] fruits = { "Banaba", "Apple", "Orang", "Grapes" };
+            ViewData["FruitsArray"] = fruits;
+
+
+           // pass list in ViewData's object
+            ViewData["SportsList"] = new List<string>()
+            {
+                "Cricket",
+                "Football",
+                "Hockey",
+                "Hally Ball"
+            };
+
+            //Retrive data from model add passes to the view
+            Employee shubh = new Employee();
+            shubh.EmpId = 11;
+            shubh.EmpName = "Shubhangi";
+            shubh.EmpDesignation = "Software Developer";
+
+            ViewData["Employee"] = shubh;
+
+            return View();
+        }
+    }
+}
---
+++ ASP.NET_MVC_Learning/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC_Learning.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+
+        public string EmpName { get; set; }
+
+        public string EmpDesignation { get; set; }
+    }
+}
