6cc4c432c68350f73dc3b142365781501b89d06d
Shubhangi <shubhangithube806@gmail.com>
2024-09-13 12:41:06 +0530

    DefferenceViewBagViewData page is added and ViewBag page is modified

---
+++ ASP.NET_MVC_Learning/Controllers/DefferenceViewBagViewDataController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Learning.Controllers
+{
+    public class DefferenceViewBagViewDataController : Controller
+    {
+        // GET: DefferenceViewBagViewData
+        public ActionResult Index()
+        {
+            ViewData["Message1"] = "Data comes from ViewData";
+            ViewBag.Message2 = "Data comes from ViewBag";
+
+            ViewData["CurrentTime1"] = DateTime.Now.ToLongTimeString(); 
+            ViewBag.CurrentTime2 = DateTime.Now.ToLongTimeString();
+
+            string[] fruits = { "Apple", "Orange", "Banana" };
+
+            ViewData["FruitsArray1"] = fruits;
+            ViewBag.FruitsArray2= fruits;
+
+
+            return View();
+        }
+    }
+}
