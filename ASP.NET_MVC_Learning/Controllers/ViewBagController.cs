2dbc942c06cbc9fbc60727bfa88dcc24f9bf7792
Shubhangi <shubhangithube806@gmail.com>
2024-09-13 11:56:44 +0530

    ViewBag page is added.

---
+++ ASP.NET_MVC_Learning/Controllers/ViewBagController.cs
@@ -0,0 +1,42 @@
+﻿using ASP.NET_MVC_Learning.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Learning.Controllers
+{
+    public class ViewBagController : Controller
+    {
+        // GET: ViewBag
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Message From ViewBag!!";  //here Message is the dynamic property.
+            ViewBag.CurrentDate = DateTime.Now.ToLongDateString();
+
+            string[] fruits = { "Apple", "Banana", "Strawberry", "Grapes" };
+            ViewBag.fruitsArray = fruits;
+
+            ViewBag.SportsList = new List<string>()
+            {
+                "Cricket",
+                "Football",
+                "Hocky",
+                "Baseball"
+            };
+
+            Student pravin = new Student();
+
+            pravin.StdId = 12;
+            pravin.StdName = "Pravin";
+            pravin.StdDesignation = "Fire Officer";
+
+            ViewBag.StuentInfo = pravin;
+
+            ViewBag.CommonMessage = "This message is accessible by both VieBag and ViewData";
+
+            return View();
+        }
+    }
+}
---
+++ ASP.NET_MVC_Learning/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC_Learning.Models
+{
+    public class Student
+    {
+        public int StdId { get; set; }
+        public string StdName { get; set; }
+        public string StdDesignation { get; set; }
+    }
+}
