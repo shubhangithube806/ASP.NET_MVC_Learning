f713fed9a778e7875c04795952a99c0a509eba91
Shubhangi <shubhangithube806@gmail.com>
2024-09-16 17:07:35 +0530

    Difference between Partial and RenderPartial

---
+++ ASP.NET_MVC_Learning/Controllers/DefferencePartialRenderPartialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Learning.Controllers
+{
+    public class DefferencePartialRenderPartialController : Controller
+    {
+        // GET: DefferencePartialRenderPartial
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
