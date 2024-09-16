35740b5fcce97d5234c1f36c5ddd366ec7c658f6
Shubhangi <shubhangithube806@gmail.com>
2024-09-16 09:44:43 +0530

    ActionLink in ASP.NET MVC

---
+++ ASP.NET_MVC_Learning/Controllers/ActionLinkController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Learning.Controllers
+{
+    public class ActionLinkController : Controller
+    {
+        // GET: ActionLink
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
