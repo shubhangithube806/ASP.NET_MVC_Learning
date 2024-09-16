17c093ad929baad61162e25bc839b5605c141202
Shubhangi <shubhangithube806@gmail.com>
2024-09-16 09:25:56 +0530

    Sessions and Temp Data in ASP.NET MVC

---
+++ ASP.NET_MVC_Learning/Controllers/SessionController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Learning.Controllers
+{
+    public class SessionController : Controller
+    {
+        // GET: Session
+        public ActionResult Index()
+        {
+            // Create object
+            ViewData["data1"] = "Data comes from viewdata";
+            ViewBag.data2 = "Data comes from viebag";
+            TempData["data3"] = "Data comes from tempdata";
+            Session["data4"] = "Data comes from session";
+
+            //CREATE ARRAY AND RETRIVE IT
+            string[] Student = { "Aryan", "Pooja", "Shubh", "Ravina" };
+
+            Session["data5"] = Student;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NET_MVC_Learning/Controllers/TempDataController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Learning.Controllers
+{
+    public class TempDataController : Controller
+    {
+        /* 1.TempData is a Dictionary object derived from the TempDataDictionary class.
+              2.Syntax TempData["objectname"] = "value";
+              3.It must be typecast before use
+              4.We use TempData to pass data from one actionmethod to another action method's view.
+        */
+
+        // GET: TempData
+        public ActionResult Index()
+        {
+            ViewData["Var1"] = "Message comes from ViewData";
+            ViewBag.Var2 = "Message comes from ViewBag";
+            TempData["Var3"] = "Message comes from TempData";
+
+            string[] games = { "Cricket", "Football", "Hallyball" };
+
+            TempData["GameArray"] = games;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            if(TempData["Var3"] != null)
+            {
+                TempData["Var3"].ToString();
+            }
+
+            return View();
+        }
+    }
+}
