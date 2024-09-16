739f598f4baec7767fc45c03d203adfc40c1c559
Shubhangi <shubhangithube806@gmail.com>
2024-09-16 14:55:06 +0530

    Strongly Typed View in ASP.NET MVC

---
+++ ASP.NET_MVC_Learning/Controllers/StronglyTypedViewController.cs
@@ -0,0 +1,42 @@
+﻿using ASP.NET_MVC_Learning.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Learning.Controllers
+{
+    public class StronglyTypedViewController : Controller
+    {
+        // GET: StronglyTypedView
+
+        public ActionResult Index()
+        {
+            Employe db = new Employe();
+            db.Id = 1234;
+            db.Name = "Shubhangi";
+            db.Age = 25;
+
+            Employe db1 = new Employe();
+            db1.Id = 1235;
+            db1.Name = "Pravin";
+            db1.Age = 24;
+
+            Employe db2 = new Employe();
+            db2.Id = 1236;
+            db2.Name = "Arya";
+            db2.Age = 21;
+
+            List<Employe> EmployeList = new List<Employe>();
+            EmployeList.Add(db);
+            EmployeList.Add(db1);
+            EmployeList.Add(db2);
+
+            /* ViewData["var1"] = db;
+             ViewBag.var2 = db; */
+
+            return View(EmployeList);
+        }
+    }
+}
---
+++ ASP.NET_MVC_Learning/Models/Employe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC_Learning.Models
+{
+    public class Employe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
