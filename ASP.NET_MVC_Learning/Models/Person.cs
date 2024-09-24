44ffc849619890cee949ad4b7f64139087401597
Shubhangi <shubhangithube806@gmail.com>
2024-09-24 18:07:09 +0530

    Scaffold Column Data Annotation.

---
+++ ASP.NET_MVC_Learning/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC_Learning.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        [ScaffoldColumn(false)]  //means when i use create template then  thier is no textfield  made for the contact field
+        public int Contact { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string Address { get; set; }
+    }
+}
