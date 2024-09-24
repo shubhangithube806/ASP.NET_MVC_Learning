5e1b8bb0bf693964987c57f29b70ed1dd5868070
Shubhangi <shubhangithube806@gmail.com>
2024-09-24 17:47:55 +0530

    DatabaseFirstApproach In Entity Framework With Crud Operations.

---
+++ EFDatabaseFirstApproach/Controllers/DatabaseFirstController.cs
@@ -0,0 +1,123 @@
+﻿using EFDatabaseFirstApproach.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFDatabaseFirstApproach.Controllers
+{
+    public class DatabaseFirstController : Controller
+    {
+        //make object of context class
+        DatabaseFirstEFDBEntities db = new DatabaseFirstEFDBEntities(); //db in this theirs is a bdset jo ki data store karvayega hamare database ke andar
+        // GET: DatabaseFirst
+        public ActionResult Index()
+        {
+            var data = db.Students.ToList();
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Students.Add(s);
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    TempData["InsertMessage"] = "<script>alert('Data Inserted !!')</script>";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["InsertMessage"] = "<script>alert('Data Not Inserted !!')</script>";
+
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var row = db.Students.Where(model => model.Id == id).FirstOrDefault();
+            return View(row);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student s) //here s is the modified data
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Entry(s).State = EntityState.Modified;
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    TempData["UpdateMessage"] = "<script>alert('Data Updated !!')</script>";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["UpdateMessage"] = "<script>alert('Data Not Updated !!')</script>";
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (id > 0)
+            {
+                var DeletedRow = db.Students.Where(model => model.Id == id).FirstOrDefault();
+                if (DeletedRow != null)
+                {
+                    db.Entry(DeletedRow).State = EntityState.Deleted;
+                    int a = db.SaveChanges();
+                    if (a > 0)
+                    {
+                        TempData["DeleteMessage"] = "<script>alert('Data Deleted !!')</script>";
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        TempData["DeleteMessage"] = "<script>alert('Data Not Deleted !!')</script>";
+                    }
+                }
+
+            }
+            return View();
+        }
+
+       /* [HttpPost]
+        public ActionResult Delete(Student s)
+        {
+            db.Entry(s).State = EntityState.Deleted;
+            int a = db.SaveChanges();
+            if (a > 0)
+            {
+                TempData["DeleteMessage"] = "<script>alert('Data Deleted !!')</script>";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["DeleteMessage"] = "<script>alert('Data Not Deleted !!')</script>";
+            }
+            return View();
+        }
+       */
+
+        public ActionResult Details(int id)
+        {
+            var row = db.Students.Where(model => model.Id == id).FirstOrDefault();
+            return View(row);
+        }
+    }
+}
