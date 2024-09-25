364cd4b968997667e762f3f6e4911ac596635969
Shubhangi <shubhangithube806@gmail.com>
2024-09-25 14:42:28 +0530

    Creating Login Form , Logout Using Session And Entity Framework.

---
+++ LoginFormEFDatabaseFirst/Controllers/HouseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginFormEFDatabaseFirst.Controllers
+{
+    public class HouseController : Controller
+    {
+        // GET: House
+        public ActionResult Index()
+        {
+            if (Session["username"] == null)   //user come to this page without login
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
---
+++ LoginFormEFDatabaseFirst/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using LoginFormEFDatabaseFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginFormEFDatabaseFirst.Controllers
+{
+    public class LoginController : Controller
+    {
+        LoginDBEntities db = new LoginDBEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User u)
+        {
+            if (ModelState.IsValid == true)   // credentials this word means password and username
+            {
+                var credentials = db.Users.Where(model => model.UserName == u.UserName && model.Password == u.Password).FirstOrDefault();
+                if (credentials == null)
+                {
+                    ViewBag.ErrorMessage = "Login Failed";
+                    return View();
+                }
+                else
+                {Session["username"] = u.UserName;
+                 return RedirectToAction("Index", "House");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();  //destroy the session
+            return RedirectToAction("Index", "Login");
+        }
+
+        public ActionResult Reset()
+        {
+            ModelState.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+
+    }
+}
