e5040a4a2ecd1f7396d9390ebd761d6f9ae275fb
Shubhangi <shubhangithube806@gmail.com>
2024-09-26 13:16:45 +0530

    41 | Shortcut of Creating CRUD Application Using DB First Approach.

---
+++ AutomaticallyCreatedCrudDBF/Controllers/HouseController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AutomaticallyCreatedCrudDBF.Models;
+
+namespace AutomaticallyCreatedCrudDBF.Controllers
+{
+    public class HouseController : Controller
+    {
+        private Entities1 db = new Entities1();
+
+        // GET: House
+        public ActionResult Index()
+        {
+            return View(db.emps.ToList());
+        }
+
+        // GET: House/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            emp emp = db.emps.Find(id);
+            if (emp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(emp);
+        }
+
+        // GET: House/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: House/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Gender,Age,Designation")] emp emp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.emps.Add(emp);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(emp);
+        }
+
+        // GET: House/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            emp emp = db.emps.Find(id);
+            if (emp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(emp);
+        }
+
+        // POST: House/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Gender,Age,Designation")] emp emp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(emp).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(emp);
+        }
+
+        // GET: House/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            emp emp = db.emps.Find(id);
+            if (emp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(emp);
+        }
+
+        // POST: House/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            emp emp = db.emps.Find(id);
+            db.emps.Remove(emp);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
