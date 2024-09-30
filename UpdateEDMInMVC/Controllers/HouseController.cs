346371fcb79966146e5331c762aecb0723c6f465
Shubhangi <shubhangithube806@gmail.com>
2024-09-30 17:52:31 +0530

    46 | How To Update Entity Data Model Using Entity Framework

---
+++ UpdateEDMInMVC/Controllers/HouseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UpdateEDMInMVC.Models;
+
+namespace UpdateEDMInMVC.Controllers
+{
+    public class HouseController : Controller
+    {
+        EDMExampleMVCEntities db = new EDMExampleMVCEntities();
+            // GET: House
+        public ActionResult Index()
+        { 
+        var data = db.Farmers.ToList();
+            return View(data);
+        }
+
+        public ActionResult Book()
+        {
+            var data = db.Books.ToList();
+            return View(data);
+        }
+    }
+}
