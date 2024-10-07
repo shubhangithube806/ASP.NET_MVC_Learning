cda490b04f2c7db4653c1c51f7f1496760c82e35
Shubhangi <shubhangithube806@gmail.com>
2024-10-07 13:52:18 +0530

    47 | Creating SignUp OR Register Page With Login, Logout & Session

---
+++ SignUpWithLoginMVC/Controllers/LoginController.cs
@@ -0,0 +1,64 @@
+﻿using SignUpWithLoginMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignUpWithLoginMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        SignupLoginEntities db = new SignupLoginEntities();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User u)
+        {
+            var user = db.Users.Where(model => model.UserName == u.UserName && model.Password == u.Password).FirstOrDefault();
+            if (user != null)
+            {
+                Session["UserId"] = u.Id.ToString();
+                Session["Username"] = u.UserName.ToString();
+                TempData["LoginSuccessMessage"] = "<script>alert('Login Successfull !!')</script>";
+                return RedirectToAction("Index", "User");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "<script>alert('Username or Password is incorrect !!')</script>";
+                return View();
+            }
+            
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User u)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Users.Add(u);
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    ViewBag.InsertMessage = "<script>alert('Registered Successfully !!')</script>";
+                    ModelState.Clear();
+                }
+                else
+                {
+                    ViewBag.InsertMessage = "<script>alert('Registration Failed !!')</script>";
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ SignUpWithLoginMVC/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignUpWithLoginMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            if (Session["UserId"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();  //meas distroy session after logout
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
