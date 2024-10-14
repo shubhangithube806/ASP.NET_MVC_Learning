f5187c9fdd7aaa3ce90bfa94b39d0d9a9abb1346
Shubhangi <shubhangithube806@gmail.com>
2024-10-14 22:20:08 +0530

    57 | Ajax BeginForm Example 2

---
+++ AjaxBeginForm2/Controllers/HouseController.cs
@@ -0,0 +1,36 @@
+﻿using AjaxBeginForm2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxBeginForm2.Controllers
+{
+    public class HouseController : Controller
+    {
+        EmployeeDBEntities db = new EmployeeDBEntities();
+        public ActionResult Index()
+        {
+            return View(db.employes.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(string q)
+        {
+            if (string.IsNullOrEmpty(q) == false)
+            {
+                List<employe> emp = db.employes.Where(model => model.Name.StartsWith(q)).ToList();
+                return PartialView("_SearchData", emp);
+            }
+            else
+            {
+                List<employe> emp = db.employes.ToList();
+                return PartialView("_SearchData", emp);
+            }
+            return View();
+        }
+
+
+    }
+}
