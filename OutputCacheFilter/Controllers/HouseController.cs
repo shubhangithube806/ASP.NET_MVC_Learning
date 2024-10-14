bae1c43a7e50f64b70133c8ed233937a4a6961ce
Shubhangi <shubhangithube806@gmail.com>
2024-10-14 14:20:07 +0530

    52 | OutputCacheFilter

---
+++ OutputCacheFilter/Controllers/HouseController.cs
@@ -0,0 +1,29 @@
+﻿using OutputCacheFilter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OutputCacheFilter.Controllers
+{
+    public class HouseController : Controller
+    {
+        FarmerDBEntities db = new FarmerDBEntities();
+
+        [OutputCache(Duration = 10, Location = System.Web.UI.OutputCacheLocation.Server)] /*The result of this index method store in Outputcache for 10 sec. */
+        public ActionResult Index()
+        {
+            ViewBag.Time = DateTime.Now.ToLongTimeString();
+            return View();
+        }
+
+        [OutputCache(Duration = 40)]  /*if any data is added in this table or list the new data is not show in the table for 40 sec.*/
+        public ActionResult GetData()
+        {
+            var data = db.FarmerInfoes.ToList();
+            return View(data);
+        }
+
+    }
+}
---
+++ OutputCacheFilter/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OutputCacheFilter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OutputCacheFilter")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e526b048-a719-4d7e-9cb5-314bd6ded2c5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
