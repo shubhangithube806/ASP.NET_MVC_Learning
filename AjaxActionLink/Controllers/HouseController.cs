21969e59059d9faa37817cba851a10f431c47f73
Shubhangi <shubhangithube806@gmail.com>
2024-10-15 11:13:19 +0530

    58 | Ajax ActionLink Method

---
+++ AjaxActionLink/Controllers/HouseController.cs
@@ -0,0 +1,36 @@
+﻿using AjaxActionLink.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxActionLink.Controllers
+{
+    public class HouseController : Controller
+    {
+        EmployeeDBEntities db = new EmployeeDBEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AllEmployees()
+        {
+            var data = db.employes.ToList();
+            return PartialView("_Employees", data);    //partialView is made in shared folder , this view is shared every where so use _ prefix infront of name
+        }
+
+        public ActionResult Top3Emp()
+        {
+            var data = db.employes.OrderByDescending(model => model.Salary).Take(3).ToList();
+            return PartialView("_Employees", data);
+        }
+
+        public ActionResult Bottom3Emp()
+        {
+            var data = db.employes.OrderBy(model => model.Salary).Take(3).ToList();
+            return PartialView("_Employees", data);
+        }
+    }
+}
