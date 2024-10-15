f75e51626f5c88557bcfb136aed94ffdec494aeb
Shubhangi <shubhangithube806@gmail.com>
2024-10-15 17:46:57 +0530

    61 |  jQuery Data Table

---
+++ JqueryDataTable/Controllers/HouseController.cs
@@ -0,0 +1,19 @@
+﻿using JqueryDataTable.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryDataTable.Controllers
+{
+    public class HouseController : Controller
+    {
+        EmployeeDBEntities db = new EmployeeDBEntities();
+        public ActionResult Index()
+        {
+            var data = db.employes.ToList();
+            return View(data);
+        }
+    }
+}
