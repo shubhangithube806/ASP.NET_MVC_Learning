6422cee6dea5f56aefa0da97094d82eb4b35871e
Shubhangi <shubhangithube806@gmail.com>
2024-10-16 13:37:35 +0530

    'Jquery Data Table MVC'   This project Is Removed

---
+++ JqueryDataTableMVC/Controllers/HouseController.cs
@@ -0,0 +1,20 @@
+﻿using JqueryDataTableMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryDataTableMVC.Controllers
+{
+    public class HouseController : Controller
+    {
+        JqueryEmployeeDBEntities db = new JqueryEmployeeDBEntities();
+        // GET: House
+        public ActionResult Index()
+        {
+            var data = db.employees.ToList();
+            return View(data);
+        }
+    }
+}
