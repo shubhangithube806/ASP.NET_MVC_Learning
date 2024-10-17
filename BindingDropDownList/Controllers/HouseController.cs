195a7e94752543b0e32ada2bc50e55282696a456
Shubhangi <shubhangithube806@gmail.com>
2024-10-17 12:59:13 +0530

    63 | Binding DropDownList With Database

---
+++ BindingDropDownList/Controllers/HouseController.cs
@@ -0,0 +1,20 @@
+﻿using BindingDropDownList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BindingDropDownList.Controllers
+{
+    public class HouseController : Controller
+    {
+        EmployeeDBEntities db = new EmployeeDBEntities();
+        public ActionResult Index()
+        {
+            List<employe> empList = db.employes.ToList();
+            ViewBag.EmployeeTbl =  new SelectList(empList, "Id", "Name");
+            return View();
+        }
+    }
+}
---
+++ BindingDropDownList/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BindingDropDownList.MvcApplication" Language="C#" %>
